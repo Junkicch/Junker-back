7f55ef04bc36530c1114e52ab2810aca8dd73d6b
Junkicch <76850261+Junkicch@users.noreply.github.com>
2021-06-25 20:17:24 -0300

    Add files via upload

---
+++ junker.js
@@ -0,0 +1,7 @@
+let img = document.getElementById("submenu")
+let navigation = document.getElementById('pages')
+
+button.addEventListener('click', function() {
+    navigation.classList.add("hello")
+navigation.style.left = "opx";
+});
